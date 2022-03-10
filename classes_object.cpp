19b5a8ebaed3c34c8ab227f8dae419e6c454904c
DEBJIT PUROHIT <DEBJITPUROHIT2001@outlook.com>
2022-03-11 01:10:24 +0530

    final

--- classes_object.cpp
+++ classes_object.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 using namespace std;
 
-class Employee{
-    public:
-    string name ="Debjit";
-    int salary = 10000;
+class Employee
+{ // class name
+public:
+    string name = "DEBJIT";
+    int salary = 100000;
 
-    void printDetails(){
-        cout<<"Name: "<< this->name <<endl;
-        cout<<"Salary: "<< this->salary<<endl;
-    }};
-
-
-int main(){
-    Employee deb;
-    
-    cout<<"the name of our first employee is "<<deb.name<<" and his salary is "<<deb.salary<<endl;
-    deb.printDetails();
+    void display()
+    {
+        cout << "the name of our first employee is " << this->name << " and his salary is " << this->salary <<endl;
+    }
+    Employee(string name, int salary,int id)
+    {
+        this->name = name;
+        this->salary = salary;
+    }
+    void printDetails()
+    {
+        cout << "Name: " << this->name << endl;
+        cout << "Salary: " << this->salary << endl;
+    }
+private:
+   int id; 
+     
+};
+int main()
+{
+    // Employee deb; //object
+    Employee sum("SUMIT", 200000,8125);
+    Employee nila("NILANJANA", 300000,8215);
+    // cout<<"the name of our first employee is "<<deb.name<<" and his salary is "<<deb.salary<<endl;
+    // deb.printDetails();
+    sum.printDetails();
+    nila.printDetails();
+    sum.display();
+    nila.display();
+    //cout<< nila.id ;// its a private variable so u cant accesss ...if u want to access this make a public variable and access it
 }
